9dfb3ffc10a1564772f2278909f220e2b946f0f5
Le0d3v <harrywilsonth01@gmail.com>
2026-03-25 14:59:08 -0600

    Adaptando ciclo escolar a produccion

--- database/seeders/CiclosEscolaresSeeder.php
+++ database/seeders/CiclosEscolaresSeeder.php
@@ -16,16 +16,10 @@ class CiclosEscolaresSeeder extends Seeder
         DB::table('ciclos_escolares')->insert([
             [
                 'nombre' => '2025 - 2026',
-                'fecha_inicio' => '2025-08-01',
+                'fecha_inicio' => '2025-09-01',
                 'fecha_fin' => '2026-07-15',
-                'activo' => false
-            ],
-            [
-                'nombre' => '2026 - 2027',
-                'fecha_inicio' => '2026-08-01',
-                'fecha_fin' => '2027-07-15',
                 'activo' => true
-            ]
+            ],
         ]);
     }
 }
